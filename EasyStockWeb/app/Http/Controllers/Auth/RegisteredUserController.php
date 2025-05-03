2ce15bfed17840c80fff9fee0e0977d35d04bb21
Kollin <carlosdelgadocampos231205@gmail.com>
2025-05-03 16:53:05 +0200

    controller registeredUser modificado

--- EasyStockWeb/app/Http/Controllers/Auth/RegisteredUserController.php
+++ EasyStockWeb/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,13 +31,18 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'telefono' => ['required', 'string', 'max:11'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ], [
+            'email.unique' => 'Este correo electrónico ya está registrado.',
+            'email.email' => 'Por favor ingresa un correo electrónico válido.',
         ]);
 
         $user = User::create([
             'name' => $request->name,
-            'email' => $request->email,
+            'telefono' => $request->telefono,
+            'email' => strtolower($request->email),
             'password' => Hash::make($request->password),
         ]);
 
@@ -45,6 +50,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('home', absolute: false));
     }
 }
