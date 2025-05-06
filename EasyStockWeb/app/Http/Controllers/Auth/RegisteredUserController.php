1c129dcd6e5d1e244859a18446a46eff21d05bcf
Kollin <carlosdelgadocampos231205@gmail.com>
2025-05-06 19:26:07 +0200

    ruta despues de registrar usuario modifcada

--- EasyStockWeb/app/Http/Controllers/Auth/RegisteredUserController.php
+++ EasyStockWeb/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -50,6 +50,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect()->route('setup.products');
+        return redirect(route('setup.products', absolute: false));
     }
 }
