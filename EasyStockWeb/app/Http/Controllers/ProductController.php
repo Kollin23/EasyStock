8bd4d17e10b69251b2add3201ef65d5af15b22d2
Kollin <carlosdelgadocampos231205@gmail.com>
2025-05-06 19:41:00 +0200

    metodo store en productController

--- EasyStockWeb/app/Http/Controllers/ProductController.php
+++ EasyStockWeb/app/Http/Controllers/ProductController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'stock' => 'required|integer|min:0',
+            'price' => 'required|numeric|min:0',
+        ]);
+
+        Product::create([
+            'user_id' => $request->user_id ?? Auth::id(),
+            'name' => $request->name,
+            'stock' => $request->stock,
+            'price' => $request->price,
+            'category_id' => $request->category_id ?? null,
+            'description' => $request->description ?? null,
+        ]);
+
+        return response()->json(['message' => 'Producto creado'], 200);
+    }
 }
