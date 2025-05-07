42366b267b6ff25f9ab3fc24f62421265ed95643
Kollin <carlosdelgadocampos231205@gmail.com>
2025-05-07 18:46:06 +0200

    imagenes para el sidebar

--- EasyStockWeb/resources/views/layouts/dashboard.blade.php
+++ EasyStockWeb/resources/views/layouts/dashboard.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>EasyStock Dashboard</title>
-    @vite('resources/css/app.css') {{-- si usas Vite --}}
+    @vite('resources/css/app.css')
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 h-screen flex">
     <!-- Sidebar -->
     <aside id="sidebar" class="group flex flex-col w-16 hover:w-56 transition-all duration-300 bg-white border-r shadow-md overflow-hidden">
-        <div class="flex items-center justify-center h-16 font-bold text-blue-900 text-xl">ES</div>
+        
         <nav class="flex-1 space-y-4 mt-4 text-gray-700 text-sm">
             <a href="#" class="group flex items-center px-4 py-2 hover:bg-gray-100">
                 <span class="material-icons">bar_chart</span>
